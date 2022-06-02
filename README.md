62bea4a7a003081311894759749953f2d19a4c68
Greg Foote <gregoryf@udel.edu>
2022-06-02 10:08:20 -0400

    Updated to Actually follow GPLv3 and GPLv2

--- README.md
+++ README.md
@@ -3,19 +3,19 @@
 ### Number converted: 82 /  209
 
 ## Description
-An attempt to convert the very good [SLALIB](http://star-www.rl.ac.uk/docs/sun67.htx/sun67.html) into 
+An attempt to convert the very good [SLALIB](http://star-www.rl.ac.uk/docs/sun67.htx/sun67.html) into
 pure python without any wrappers using [pyslalib](https://github.com/scottransom/pyslalib) as a base.
 
 ## Workflow
-`convert.py` identifies the components of a given Fortran line of code with `pyparsing` then attempts to 
-convert it into a line of python code and then finally uses `black` to attempt to format the entire transformed 
-function. While this process is not perfect, it is close enough that a human should be able to make it into decent 
+`convert.py` identifies the components of a given Fortran line of code with `pyparsing` then attempts to
+convert it into a line of python code and then finally uses `black` to attempt to format the entire transformed
+function. While this process is not perfect, it is close enough that a human should be able to make it into decent
 python with little effort and fix any errors.
 
 
 ## Legal
-The original `SLALIB` was released with [GNUv2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html); while
-[pyslalib](https://github.com/scottransom/pyslalib) doesn't have a LICENSE file. Based on the conditions 
-in the GNUv2 I am assuming `pyslalib` was released under GNUv2 as well, and I am releasing this code under 
-GNUv3. The various `LICENSE` files are for each set of code being used and manipulated. I also took the liberty of 
+The original `SLALIB` was released with [GPLv2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html); while
+[pyslalib](https://github.com/scottransom/pyslalib) doesn't have a LICENSE file. Based on the conditions
+in the GPLv2 I am assuming `pyslalib` must be released under GPLv2 as well, and I am releasing this code under
+GPLv3. The various `LICENSE` files are for each set of code being used and manipulated. I also took the liberty of
 updating the `SLALIB` license to the modern GNUv2 as opposed to the very outdated version (It had the wrong address).
--- convert.py
+++ convert.py
@@ -1,3 +1,56 @@
+"""
+PyPySLALib is licensed under GPLv3; 
+PySLALib and SLALib are licensed under GPLv2;
+The legal notices are below
+
+---------------------------------------------------------------
+PyPySLALib, a full python conversion of the astrometrics library SLALib
+Copyright (C) 2022  Gregory Foote
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+---------------------------------------------------------------
+f2py-generated wrappers for SLALIB
+Copyright (C) 2010 Scott M. Ransom
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+---------------------------------------------------------------
+SLALIB is a library of routines intended to make accurate and reliable positional-astronomy applications easier to write
+Copyright (C) 1995 P.T.Wallace; Starlink; Rutherford Appleton Laboratory
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+---------------------------------------------------------------
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 import argparse
 import json
 import os
